c91bc011605d002231262439b1decb4a62cc1e79
denis <denistrator@yandex.ua>
2016-12-30 19:36:49 +0200

    add education, expirience, portfolio, skills sections

---
+++ markup/components/portfolio/data/data.js
@@ -0,0 +1,46 @@
+var data = {
+	portfolio: [
+		{
+			imgPath: "project-1",
+			link: "http://denistrator.esy.es/mountains",
+			title: "Mountains",
+			githubLink: "https://github.com/denistrator/training-01-mountains",
+			mod: ""
+		},
+		{
+			imgPath: "project-2",
+			link: "http://denistrator.esy.es/modest",
+			title: "Modest",
+			githubLink: "https://github.com/denistrator/spalah-lessons--lesson-8",
+			mod: ""
+		},
+		{
+			imgPath: "project-3",
+			link: "http://denistrator.esy.es/tajam",
+			title: "Tajam",
+			githubLink: "https://github.com/denistrator/spalah-lessons--lesson-7",
+			mod: ""
+		},
+		{
+			imgPath: "project-4",
+			link: "http://denistrator.esy.es/yellow_moon",
+			title: "Yellow Moon",
+			githubLink: "https://github.com/denistrator/spalah-lessons--lesson-6",
+			mod: ""
+		},
+		{
+			imgPath: "project-5",
+			link: "http://denistrator.esy.es/Freebie_Page_Atech/",
+			title: "Atech",
+			githubLink: "",
+			mod: "old"
+		},
+		{
+			imgPath: "project-no-image",
+			link: "http://denistrator.esy.es/extended/",
+			title: "Extended",
+			githubLink: "",
+			mod: "old"
+		}
+	]
+};
