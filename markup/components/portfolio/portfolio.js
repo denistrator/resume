269a93e1bd0ade8a3f8ed13467b1145732280de3
denis <denistrator@yandex.ua>
2017-03-12 20:41:12 +0200

    fix wrong expanded height

--- markup/components/portfolio/portfolio.js
+++ markup/components/portfolio/portfolio.js
@@ -3,16 +3,12 @@
     var collapsedHeight = document.querySelector(".portfolio-collapse-pos").offsetTop +
    						  document.querySelector(".portfolio-collapse-pos").offsetHeight;
 
-    blockToCollapse.style.height = collapsedHeight +"px";      
-    // setTimeout(function() {
-    //   blockToCollapse.style.height = collapsedHeight +"px";      
-    // }, 3000);
+    blockToCollapse.style.height = collapsedHeight +"px";
 
     var expander = document.querySelector(".portfolio-expander");
     expander.addEventListener('click', function() {
-   	  var initialHeight =  blockToCollapse.scrollHeight;
-    	blockToCollapse.style.height = initialHeight - expander.offsetHeight +"px";
+        blockToCollapse.style.height = blockToCollapse.scrollHeight +"px";
     	expander.classList.add("hide");
-    }, false);
+    });
 })();
 
