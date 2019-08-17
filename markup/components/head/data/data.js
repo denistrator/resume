7224ac911a77ef838b8456fa852c2428a55a227e
denistrator <denistrator@yandex.ua>
2019-08-17 20:02:46 +0300

    add page description

--- markup/components/head/data/data.js
+++ markup/components/head/data/data.js
@@ -1,8 +1,9 @@
 var data = {
     head: {
         defaults: {
-            title: 'Денис Белевцов - Резюме',
-            useSocialMetaTags: true
+            pageTitle: 'Belevtsov Denis - Resume',
+            useSocialMetaTags: true,
+            pageDescription: 'Belevtsov Denis - Resume. List of Skills, info about Education, Experience and Portfolio. Currently not open to suggestions'
         }
     }
 };
