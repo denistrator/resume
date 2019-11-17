ac431aa686b84c422165d743467c0c9a1b7ca42e
denistrator <denistrator@yandex.ua>
2019-11-17 13:24:28 +0200

    update project build

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,47 +1,48 @@
-(function () {
-    var $readingModeToggle = document.querySelector('.reading-mode-toggle');
-
-    $readingModeToggle.addEventListener('click', function () {
-        var $page = document.querySelector('.page');
-
-        $page.classList.toggle('light');
-        $page.classList.toggle('dark');
-    }, false);
-})();
+"use strict";
 
 (function () {
-    var $langToggle = document.querySelector('.lang-toggle');
-    var $langToggleText = document.querySelector('.lang-toggle-text');
-    var $root = document.querySelector('html');
+  var $root = document.querySelector('html'),
+      $langToggle = document.querySelector('.lang-toggle');
+  var languages = ['en', 'ru'];
+  $langToggle.addEventListener('click', switchLanguages, false);
 
-    $langToggle.addEventListener('click', function () {
-        var currentLang = $root.getAttribute('lang');
-        var lang, locale;
-        switch (currentLang) {
-            case 'en':
-                lang = 'ru';
-                locale = 'Ru';
-                break;
-            default:
-                lang = 'en';
-                locale = 'En';
-        }
-        $root.setAttribute('lang', lang);
-        $langToggleText.innerHTML = locale;
-    }, false);
+  function switchLanguages() {
+    $root.setAttribute('lang', getNextLanguage());
+  }
+
+  function getNextLanguage() {
+    var currentLanguageIndex = languages.indexOf($root.getAttribute('lang'));
+    return languages[currentLanguageIndex + 1] || languages[0];
+  }
 })();
+"use strict";
 
-(function() {
-	var blockToCollapse = document.querySelector(".portfolio-list");
-    var collapsedHeight = document.querySelector(".portfolio-collapse-pos").offsetTop +
-   						  document.querySelector(".portfolio-collapse-pos").offsetHeight;
+(function () {
+  var $readingModeToggle = document.querySelector('.reading-mode-toggle');
+  $readingModeToggle.addEventListener('click', switchReadingMode, false);
 
-    blockToCollapse.style.height = collapsedHeight +"px";
-
-    var expander = document.querySelector(".portfolio-expander");
-    expander.addEventListener('click', function() {
-        blockToCollapse.style.height = blockToCollapse.scrollHeight +"px";
-    	expander.classList.add("hide");
-    });
+  function switchReadingMode() {
+    var $page = document.querySelector('.page');
+    $page.classList.toggle('light');
+    $page.classList.toggle('dark');
+  }
 })();
+"use strict";
 
+(function () {
+  var $portfolioList = document.querySelector('.portfolio-list'),
+      $portfolioListCollapsePos = $portfolioList.querySelector('.portfolio-collapse-pos'),
+      $portfolioListExpander = $portfolioList.querySelector('.portfolio-expander'),
+      portfolioListCollapsedHeight = $portfolioListCollapsePos.offsetTop + $portfolioListCollapsePos.offsetHeight;
+  collapsePortfolioList();
+  $portfolioListExpander.addEventListener('click', expandPortfolioList);
+
+  function collapsePortfolioList() {
+    $portfolioList.style.height = portfolioListCollapsedHeight + 'px';
+  }
+
+  function expandPortfolioList() {
+    $portfolioList.style.height = $portfolioList.scrollHeight + 'px';
+    $portfolioListExpander.classList.add('hide');
+  }
+})();
