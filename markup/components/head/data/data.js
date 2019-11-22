ce5cbbe21526c7b4e90cc71475965dcc9f8d8bd6
denistrator <denistrator@yandex.ua>
2019-11-22 12:28:50 +0200

    add "theme_color" in manifest

--- markup/components/head/data/data.js
+++ markup/components/head/data/data.js
@@ -3,7 +3,7 @@ const data = {
         defaults: {
             pageTitle: 'Belevtsov Denis - Resume',
             pageDescription: 'Belevtsov Denis - Resume. List of Skills, info about Education, Experience and Portfolio. Currently not open to suggestions',
-            themeColor: '#f0eef1'
+            themeColor: '#f0eef1' // also used in manifest.json
         }
     }
 };
