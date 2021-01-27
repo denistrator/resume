d840e58f6bd526ae0035c50abfc5921ef84b4fe1
denistrator <denistrator@yandex.ua>
2021-01-27 08:09:03 +0200

    correct path to icomoon.woff

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,6 +1,30 @@
 "use strict";
 
 (function () {
+  var $list = document.querySelector('.portfolio-list'),
+      $listHiddenItems = $list.querySelectorAll('.portfolio-item.hidden'),
+      $listCollapsePos = $list.querySelector('.portfolio-collapse-pos'),
+      $listExpander = document.querySelector('.portfolio-expander'),
+      listCollapsedHeight = $listCollapsePos.offsetTop + $listCollapsePos.offsetHeight;
+  collapsePortfolioList();
+  $listExpander.addEventListener('click', expandPortfolioList);
+
+  function collapsePortfolioList() {
+    $list.style.height = listCollapsedHeight + 'px';
+  }
+
+  function expandPortfolioList() {
+    Array.from($listHiddenItems).forEach(function (e) {
+      e.classList.remove('hidden');
+    });
+    $list.style.height = $list.scrollHeight + 'px';
+    $list.classList.add('expanded');
+    $listExpander.classList.add('hidden');
+  }
+})();
+"use strict";
+
+(function () {
   var $root = document.querySelector('html'),
       $langToggle = document.querySelector('.lang-toggle');
   var languages = ['en', 'ru'];
@@ -25,29 +49,5 @@
     var $page = document.querySelector('.page');
     $page.classList.toggle('light');
     $page.classList.toggle('dark');
-  }
-})();
-"use strict";
-
-(function () {
-  var $list = document.querySelector('.portfolio-list'),
-      $listHiddenItems = $list.querySelectorAll('.portfolio-item.hidden'),
-      $listCollapsePos = $list.querySelector('.portfolio-collapse-pos'),
-      $listExpander = document.querySelector('.portfolio-expander'),
-      listCollapsedHeight = $listCollapsePos.offsetTop + $listCollapsePos.offsetHeight;
-  collapsePortfolioList();
-  $listExpander.addEventListener('click', expandPortfolioList);
-
-  function collapsePortfolioList() {
-    $list.style.height = listCollapsedHeight + 'px';
-  }
-
-  function expandPortfolioList() {
-    Array.from($listHiddenItems).forEach(function (e) {
-      e.classList.remove('hidden');
-    });
-    $list.style.height = $list.scrollHeight + 'px';
-    $list.classList.add('expanded');
-    $listExpander.classList.add('hidden');
   }
 })();
