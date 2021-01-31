9a6d7042abbfb852ad1225e59e8d6f0c2a61f54d
denistrator <denistrator@yandex.ua>
2021-01-31 20:13:48 +0200

    split skills component data and template

---
+++ markup/components/skills/data/data.js
@@ -0,0 +1,386 @@
+const data = {
+    skillAreas: [
+        {
+            title: {
+                default: 'Magento 2:'
+            },
+            skillGroups: [
+                [
+                    {
+                        label: {
+                            en: 'XML Layout',
+                            ru: 'XML Лейаут'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Fallback/Inheritance system',
+                            ru: 'Fallback/Inheritance система'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'Ui lib'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Blank & Luma'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Custom modules development',
+                            ru: 'Разработка модулей'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: '3rd-party modules customization <br/> (Mirasvit, Amasty, Ubertheme, etc)',
+                            ru: 'Кастомизация сторонних модулей <br/> (Mirasvit, Amasty, Ubertheme, и пр)'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'PHP plugins',
+                            ru: 'PHP плагины'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'ViewModels'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Widgets (widget.xml)',
+                            ru: 'Виджеты (widget.xml)'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Ui components',
+                            ru: 'Ui компоненты'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Js Layout'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'JS mixins',
+                            ru: 'JS максины'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'r.js bundling (e.g. magepack)',
+                            ru: 'r.js бандлинг (напр. magepack)'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Emails customization',
+                            ru: 'Кастомизация писем'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'RTL themes implementation',
+                            ru: 'Реализация RTL тем'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Magento 1 (themes, modules)',
+                            ru: 'Magento 1 (темы, модули)'
+                        }
+                    }
+                ]
+            ]
+        },
+        {
+            title: {
+                en: 'Front-end:',
+                ru: 'Фронт-энд:'
+            },
+            skillGroups: [
+                [
+                    {
+                        label: {
+                            default: 'HTML'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'CSS'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'SCSS'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'LESS'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'BEM'
+                        }
+                    },
+                    {
+                        label: {
+                            en: 'Modular markup',
+                            ru: 'Модульная вёрстка'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Semantic markup',
+                            ru: 'Семантическая вёрстка'
+                        }
+                    },
+                    {
+                        label: {
+                            en: 'Accessibility',
+                            ru: 'Доступность'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'RTL Implementation',
+                            ru: 'Реализация RTL'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'JS'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'jQuery'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Knockout'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'Require JS'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'JS Bundling'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'Handlebars'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Jade'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'NPM'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Grunt'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Gulp'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'Photoshop'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Zeplin'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Avocode'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'SVG'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Font Icons'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'Pixel perfect'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Optimization for Google Pagespeed <br> (this page has 100 score)',
+                            ru: 'Оптимизация для Google Pagespeed <br> (результат этой страницы 100 баллов)'
+                        }
+                    }
+                ]
+            ]
+        },
+        {
+            title: {
+                en: 'Back-end:',
+                ru: 'Бэк-энд:'
+            },
+            skillGroups: [
+                [
+                    {
+                        label: {
+                            default: 'Composer'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            default: 'PHP'
+                        }
+                    },
+                    {
+                        label: {
+                            en: 'PHP Debug',
+                            ru: 'PHP Дебаг'
+                        }
+                    },
+                    {
+                        label: {
+                            en: 'PHP DOM <br/> (parsing &amp; modifying html on server side)',
+                            ru: 'PHP DOM <br/> (работа с html dom на стороне сервера)'
+                        }
+                    }
+                ],
+                [
+                    {
+                        label: {
+                            en: 'Basic MySQL',
+                            ru: 'Базовый MySQL'
+                        }
+                    }
+                ]
+            ]
+        },
+        {
+            title: {
+                en: 'Misc:',
+                ru: 'Прочее:'
+            },
+            skillGroups: [
+                [
+                    {
+                        label: {
+                            default: 'Jira'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Git'
+                        }
+                    }
+                ],
+
+                [
+                    {
+                        label: {
+                            default: 'Linux'
+                        }
+                    },
+                    {
+                        label: {
+                            default: 'Win 10 + WSL'
+                        }
+                    }
+                ],
+
+                [
+                    {
+                        label: {
+                            en: 'Semantic versioning',
+                            ru: 'Семант. верс.'
+                        }
+                    },
+                    {
+                        label: {
+                            en: 'Keeping a changelog',
+                            ru: 'Ведение changelog'
+                        }
+                    }
+                ]
+            ]
+        }
+    ]
+};
