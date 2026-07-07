d546a45b76dcad229d0c032c76fc3dd99213f7ac
denistrator <denistrator@yandex.com>
2026-07-07 22:31:37 +0300

    feat: add employment section

---
+++ markup/components/employment/data/data.js
@@ -0,0 +1,68 @@
+const data = {
+    employment: {
+        title: {
+            en: 'Employment',
+            ru: 'Трудовая деятельность',
+            uk: 'Трудова діяльність'
+        },
+        description: {
+            en: 'Career history and professional engagements',
+            ru: 'История карьеры и профессиональная деятельность',
+            uk: 'Історія кар\'єри та професійна діяльність'
+        },
+        items: [
+            {
+                title: {
+                    en: 'Freelance Developer',
+                    ru: 'Фриланс-разработчик',
+                    uk: 'Фриланс-розробник'
+                },
+                company: {
+                    en: 'Self-employed',
+                    ru: 'Самозанятый',
+                    uk: 'Самозайнятий'
+                },
+                startDate: '2026',
+                endDate: null,
+                present: {
+                    en: 'present',
+                    ru: 'наст.',
+                    uk: 'наст.'
+                }
+            },
+            {
+                title: {
+                    en: 'Senior Magento 2 Developer',
+                    ru: 'Senior Magento 2 разработчик',
+                    uk: 'Senior Magento 2 розробник'
+                },
+                company: {
+                    en: 'StudioRaz',
+                    ru: 'StudioRaz',
+                    uk: 'StudioRaz'
+                },
+                location: {
+                    en: 'Israel',
+                    ru: 'Израиль',
+                    uk: 'Ізраїль'
+                },
+                startDate: '2017',
+                endDate: '2026'
+            },
+            {
+                title: {
+                    en: 'Freelance Developer',
+                    ru: 'Фриланс-разработчик',
+                    uk: 'Фриланс-розробник'
+                },
+                company: {
+                    en: 'Self-employed',
+                    ru: 'Самозанятый',
+                    uk: 'Самозайнятий'
+                },
+                startDate: '2015',
+                endDate: '2017'
+            }
+        ]
+    }
+};
